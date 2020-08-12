8cdfb12017006429c4862d1b1809c2fa3f572a44
wk <wazohu@0223gmail.com>
2020-08-12 09:08:00 +0800

    尝试按照代码规范修改自己的过于弱智代码 XD

--- 001.py
+++ 001.py
@@ -1,26 +1,28 @@
 # -*- coding: utf-8 -*-
-list = []
+#sorted函数排序方法
+list1 = []
 for i in range(5):
-    a = int(input('请输入整数:'))
-    list.append(a)
-y=sorted(list)
-print(list)
-print(y)
+    num1 = int (input('请输入整数:'))
+    list1.append(num1)
+list2=sorted(list1)
+print(list1)
+print(list2)
 input('press any key to...')
 
-#增加了冒泡排序
-list = []
+#增加了冒泡排序法
+list3 = []
 print ('请输入十个整数：')
 for i in range(10):
     print ('输入第%d个整数：'%i)
-    a = input()
-    list.append(a)
-print (list)
+    num2 = int input()
+    list3.append(num2)
+print (list3)
  
 for i in range(9):
     for j in range(9 - i):
-        if list[j] > list[j+1]:
-	        list[j], list[j + 1] = list[j + 1], list[j]
-    print (list)
+        if list3[j] > list3[j+1]:
+	        list3[j], list3[j + 1] = list3[j + 1], list3[j]
+print (list3)
+input ('press any key to...')
 
     
